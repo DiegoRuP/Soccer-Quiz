1ea1a6855379163580d48bd396cbf58fa5e804c0
Ivan Francisco <151671816+IvanBayoR@users.noreply.github.com>
2024-03-03 18:21:25 -0600

    Imagenes Aleatorias
    Se logro con exito hacer que de 9 imagenes solo aparezcan 3 de forma aleatoria

--- juego.js
+++ juego.js
@@ -1,3 +1,5 @@
+//PARA ACTUALIZAR EL TIEMPO
+
 const contadorTiempo = document.getElementById("contadorTiempo");
 
 let segundos = 0;
@@ -10,3 +12,40 @@ function actualizarTiempo() {
     contadorTiempo.textContent = tiempoFormateado;
 }
 setInterval(actualizarTiempo, 1000);
+
+//PARA APARECER 3 JUGADORES DE FORMA ALEATORIA
+const cajaimagenes = document.getElementById("cajaimagenes");
+
+const imagenesJugadores = [
+  "src/bellingham.png",
+  "src/benzema.png",
+  "src/chicharo.png",
+  "src/cr7.png",
+  "src/dibu.png",
+  "src/haaland.png",
+  "src/marco.png",
+  "src/mbappe.png",
+  "src/messi.png"
+];
+
+function obtenerImagenesAleatorias() {
+  const imagenesAleatorias = imagenesJugadores.slice();
+  for (let i = imagenesAleatorias.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [imagenesAleatorias[i], imagenesAleatorias[j]] = [imagenesAleatorias[j], imagenesAleatorias[i]];
+  }
+  return imagenesAleatorias.slice(0, 3);
+}
+
+function mostrarImagenesAleatorias() {
+  const imagenesAleatorias = obtenerImagenesAleatorias();
+    cajaimagenes.innerHTML = "";
+    imagenesAleatorias.forEach(imagen => {
+    const img = document.createElement("img");
+    img.src = imagen;
+    img.alt = "Jugador";
+    cajaimagenes.appendChild(img);
+  });
+}
+mostrarImagenesAleatorias();
+
